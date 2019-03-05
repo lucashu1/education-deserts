7440ce292103390df42abc7e469e44854f534e01
Nikhil Sinha <nikhilsinha@outlook.com>
2019-03-05 14:40:12 -0800

    Convert to bounding box

--- helper.py
+++ helper.py
@@ -9,11 +9,13 @@ degrees_to_radians = math.pi/180.0
 radians_to_degrees = 180.0/math.pi
 
 def change_in_latitude(miles):
-    "Given a distance north, return the change in latitude."
     return (miles/earth_radius)*radians_to_degrees
 
 def change_in_longitude(latitude, miles):
-    "Given a latitude and a distance west, return the change in longitude."
-    # Find the radius of a circle around the earth at given latitude.
     r = earth_radius*math.cos(latitude*degrees_to_radians)
     return (miles/r)*radians_to_degrees
+
+def get_bounding_box(lat, lon):
+    lat_change = change_in_latitude(50)
+    long_change = change_in_longitude(lat, 50)
+    return (lat-lat_change, lon-long_change, lat+lat_change, lon-long_change)
