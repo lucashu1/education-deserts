cc4be206fbdb78eac99b0093c45cf0e8ea3d3aa8
Nikhil Sinha <nikhilsinha@outlook.com>
2019-03-05 15:06:37 -0800

    Fill out list generation algorithm

--- index_builder.py
+++ index_builder.py
@@ -1,7 +1,29 @@
 import rtree
 import shapely
+from helper import get_bounding_box
 
-def build(centroids):
+def build(centroids, colleges):
     centroid_index = index.Index()
+    college_index = index.Index()
+    for geo_id in centroids:
+        lat, lon = centroids[geo_id]
+        centroid_index.insert(geo_id, (lat, lon, lat, lon))
+
+    #store centroid index to file (see http://toblerity.org/rtree/tutorial.html)
     
-    #store index to file (see http://toblerity.org/rtree/tutorial.html)
+    centroid_50_miles = {}
+
+    for geo_id in centroids:
+        lat, lon = centroids[geo_id]
+        bb = get_bounding_box(lat, lon)
+        within_50_miles = list(centroid_index.intersection(bb))
+        centroid_50_miles[geo_id] = {'list': within_50_miles, 'edu_desert':1}
+
+    for id in colleges:
+        lat, lon = colleges[id]
+        bb = get_bounding_box(lat, lon)
+        within_50_miles = list(centroid_index.intersection(bb))
+        for geo_id in within_50_miles:
+            centroid_50_miles[geo_id]['edu_desert'] = 0
+
+    #store centroid 50 mile dict to file
