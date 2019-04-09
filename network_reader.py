4046c493fb1ee27e8724db2a576cb0c94ce659a9
Nikhil Sinha <nikhilsinha@outlook.com>
2019-04-08 22:43:30 -0700

    Fix some bugs

--- network_reader.py
+++ network_reader.py
@@ -1,34 +1,30 @@
 import csv
 import json
 import bisect
+import time
 
 class Node:
-    def __init__(val, geo_id):
+    def __init__(self, geo_id, val = 0):
         self.geo_id = geo_id
         self.value = val
         self.neighbors = []
 
-    def __init__(geo_id):
-        self.geo_id = geo_id
-        self.value = 0
-        self.neighbors = []
-
-    def set_val(val):
+    def set_val(self, val):
         self.value = val
 
-    def get_val():
+    def get_val(self):
         return self.value
 
-    def add_neighbor(neighbor):
+    def add_neighbor(self, neighbor):
         self.neighbors.append(neighbor)
 
-    def get_neighbors():
+    def get_neighbors(self):
         return self.neighbors
 
-    def get_id():
+    def get_id(self):
         return self.geo_id
 
-    def get_total_value(taken):
+    def get_total_value(self, taken):
         val = self.value if self.geo_id not in taken else 0.0
         for neighbor in self.neighbors:
             if not taken[neighbor]:
@@ -36,18 +32,18 @@ class Node:
         return val
 
 class Network:
-    def __init__():
+    def __init__(self):
         self.nodes = {}
         self.taken = {}
         self.sorted_list = []
 
-    def add_node(node):
+    def add_node(self, node):
         self.nodes[node.get_id()] = node
 
-    def has_node(geo_id):
+    def has_node(self, geo_id):
         return geo_id in self.nodes
 
-    def add_neighbors(geo_id, neighbor_ids):
+    def add_neighbors(self, geo_id, neighbor_ids):
         for neighbor in neighbor_ids:
             if neighbor in self.nodes:
                 self.nodes[geo_id].add_neighbor(self.nodes[neighbor])
@@ -55,21 +51,21 @@ class Network:
                 self.add_node(Node(neighbor))
                 self.nodes[geo_id].add_neighbor(self.nodes[neighbor])
 
-    def get_val(arr):
+    def get_val(self, arr):
         return arr[1]
 
-    def initial_sort():
-        self.sorted_list = [[node.get_id(), node.get_total_value([])] for node in self.nodes]
+    def initial_sort(self):
+        self.sorted_list = [[n_id, node.get_total_value([])] for n_id, node in self.nodes.items()]
         self.sorted_list.sort(key=self.get_val, reverse=true)
 
-    def take():
+    def take(self):
         node_id = self.sorted_list[0][0]
         self.taken[node_id] = None
         for neighbor in self.nodes[node_id].get_neighbors():
             self.taken[neighbor] = None
         return node_id, self.nodes[node_id].get_total_value(self.taken)
 
-    def sort_single():
+    def sort_single(self):
         node_summary = self.sorted_list[0]
         node_summary[1] = self.nodes[node_summary[0]].get_total_value(self.taken)
         self.sorted_list = self.sorted_list[1:]
@@ -79,30 +75,44 @@ class Network:
 
 def read_network(json_filename, csv_file, census_file, compute_weight):
     json_file = open(json_filename)
+    start = time.time()
     nodes = json.load(json_file)
+    end = time.time()
+    print('Reading JSON took ' + str(end - start) + ' seconds')
     weights = {}
     net = Network()
     features = {}
+    
+    start = time.time()
     with open(census_file) as featuresfile:
         featuresreader = csv.DictReader(featuresfile, delimiter=',')
         for row in featuresreader:
-            salary = (row['Median Household Income (In 2017 Inflation Adjusted Dollars)']*row['Households:.3'])/row['Population 16 Years and Over: in Labor Force']
-            features[row['geoID']] = [row['Total Population:'], salary, row['Pct. Population 25 Years and Over: Bachelor\'s Degree']]
-        
-    with open(csv_file) as weightfile:
-        weightreader = csv.reader(weightfile, delimiter=',')
-        for row in weightreader[1:]:
-            pop, salary, pct = features[row[0]]
-            try:
-                weights[row[0]] = compute_weight(pop, salary, pct, float(row[1]))
-            except:
-                weights[row[0]] = compute_weight(pop, salary, pct, 0.0)
+            working_pop = float(row['Population 16 Years and Over: in Labor Force'])
+            salary = (float(row['Median Household Income (In 2017 Inflation Adjusted Dollars)'])*float(row['Households:.3']))/float(row['Population 16 Years and Over: in Labor Force']) if working_pop > 0 else float(row['Median Household Income (In 2017 Inflation Adjusted Dollars)'])
+            features[row['geoID']] = [row['Total Population:'], salary, float(row['Pct. Population 25 Years and Over: Bachelor\'s Degree'])]
+    end = time.time()
+    print('Reading features CSV took ' + str(end - start) + ' seconds')
 
+    start = time.time()
+    with open(csv_file) as weightfile:
+        weightreader = csv.DictReader(weightfile, delimiter=',')
+        for row in weightreader:
+            pop, salary, pct = features[row['geoID']]
+            try:
+                weights[row['geoID']] = compute_weight(pop, salary, pct, float(row['pred_pct_bachelors']))
+            except:
+                weights[row['geoID']] = compute_weight(pop, salary, pct, 0.0)
+    end = time.time()
+    print('Reading weights CSV took ' + str(end - start) + ' seconds')
+
+    start = time.time()
     for key in nodes.keys():
         if net.has_node(key):
             net.nodes[key].set_val(weights[key] if key in weights else 0.0)
         else:
             net.add_node(Node(key, weights[key] if key in weights else 0.0))
         net.add_neighbors(key, nodes[key])
+    end = time.time()
+    print('Building graph took ' + str(end - start) + ' seconds')
 
     return net
--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,5 @@
 from network_reader import read_network
+import time
 
 def compute_added_average_salary(_, salary, pct, pct_pred):
     diff = (pct_pred - pct) if (pct_pred-pct) > 0 else 0
@@ -9,17 +10,28 @@ def compute_added_total_salary(pop, salary, pct, pct_pred):
     return (salary*(1-diff)*pop + 50516*diff*pop) - salary*pop
 
 def top_k_locations(graph_file, prediction_file, census_file, k, compute_weight):
+    start = time.time()
     network = read_network(graph_file, prediction_file, census_file, compute_weight)
+    end = time.time()
+    print('Reading network took ' + str(end - start) + ' seconds')
+    start = time.time()
     network.initial_sort()
+    end = time.time()
+    print('Initial sort took ' + str(end - start) + ' seconds')
     outtext = ""
     for i in range(k)-1:
+        start = time.time()
         top, value = network.take()
         outtext += top + ": " + str(value) + "\n"
-        outtext
         while(True):
             if network.sort_single():
                 break
+        end = time.time()
+        print("Iteration " + str(i) + " took " + str(end-start) + " seconds")
+    start = time.time()
     top, value = network.take()
+    end = time.time()
+    print("Iteration " + str(k) + " took " + str(end-start) + " seconds")
     outtext += top + ": " + str(value)
     f = open("output.txt", "w+")
     f.write(outtext)
